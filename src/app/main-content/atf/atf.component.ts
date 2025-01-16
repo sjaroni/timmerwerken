9416aeaa1107581d4a618162357666543d9dd756
Stefan Jaroni <sjaroni@outlook.de>
2025-01-16 23:25:13 +0100

    style: add new components

---
+++ src/app/main-content/atf/atf.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atf',
+  imports: [],
+  templateUrl: './atf.component.html',
+  styleUrl: './atf.component.scss'
+})
+export class AtfComponent {
+
+}
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NavComponent } from "../shared/components/nav/nav.component";
+import { AtfComponent } from "./atf/atf.component";
+import { SkillComponent } from "./skill/skill.component";
 
 @Component({
   selector: 'app-main-content',
-  imports: [NavComponent],
+  imports: [NavComponent, AtfComponent, SkillComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
