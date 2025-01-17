4d19c0018b3c02d9a772080e65088850bc1abb5d
Stefan Jaroni <sjaroni@outlook.de>
2025-01-17 20:47:08 +0100

    style: add footer, new light logo, improve code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/components/header/header.component";
 import { FooterComponent } from "./shared/components/footer/footer.component";
+import { NavComponent } from "./shared/components/nav/nav.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'heider-timmerwerken';  
+export class AppComponent implements OnInit{
+  title = 'heider-timmerwerken';
+  
+  ngOnInit(): void {
+    console.log('%cWelkom op mijn site ❤️', 'color: #11a2d7; font-size: 16px; font-weight: bold;');
+  }
 }
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { NavComponent } from "../shared/components/nav/nav.component";
 import { AtfComponent } from "./atf/atf.component";
 import { SkillComponent } from "./skill/skill.component";
 
 @Component({
   selector: 'app-main-content',
-  imports: [NavComponent, AtfComponent, SkillComponent],
+  imports: [AtfComponent, SkillComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
